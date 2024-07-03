604d32d83239924e4efa09cbffc7040cd108f28b
TKU-FCP-411631137 <mfc930223@gmail.com>
2024-07-03 11:45:40 +0800

    ch__4_5

---
+++ ch2__4.java
@@ -0,0 +1,14 @@
+public class ch2__4 {
+    
+    public static void main(String[] args){
+        int num1=2;
+        int num2=3;
+
+        System.out.println("I have " +num1+" dogs");
+        System.out.println("You have " +num2+" dogs");
+
+
+
+    }
+
+}
---
+++ ch2___5.java
@@ -0,0 +1,10 @@
+public class ch2___5 {
+    
+    public static void main(String[] args){
+        int a=2;    //宣告整數變數a，並設值為2
+        int b=3;    //宣告整數變數b，並設值為3
+        int c=a+b;
+        System.out.println("a+b=" +c);
+
+    }
+}
