827bc27346e29990d424298de55874eff1185611
TKU-FCP-411631137 <mfc930223@gmail.com>
2024-07-03 11:44:12 +0800

    Create ch2__6.java

---
+++ ch2__6.java
@@ -0,0 +1,11 @@
+public class ch2__6 {
+    public static void main(String[] args){
+        int a=12;
+        int b=26;
+        System.out.println(a+"+"+b+"="+(a+b));
+        System.out.println("a+b="+(a+b));
+        System.out.println(a+b);
+
+
+    }
+}
