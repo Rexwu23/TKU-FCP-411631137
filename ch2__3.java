d2d2b89be12662ada60e4993cbebf5c10983e8f4
TKU-FCP-411631137 <mfc930223@gmail.com>
2024-07-03 11:00:12 +0800

    ch2_3

--- ch2__3.java
+++ ch2__3.java
@@ -1,7 +1,7 @@
-public class ch2__3 {
+public class ch2__3 {//定義類別
     public static void main(String[] args){
-        int a=2, b=5;
-        System.out.println(a+"*"+b+"+"+(a+b));
+        int a=2, b=5;       //宣告變數a和b
+        System.out.println(a+"*"+b+"="+(a*b));
 
 
     }
