4885cdea2f4478df29f1a66231a9ba5e6abc6a40
TKU-FCP-411631137 <mfc930223@gmail.com>
2024-07-08 12:36:00 +0800

    CH3

---
+++ ch3__1.java
@@ -0,0 +1,9 @@
+public class ch3__1 {
+    public static void main(String[] args){
+        int num = 3;
+        char ch = 'C';
+        System.out.println(num+" is an interger");
+        System.out.println(ch+" is a agaracter");
+
+    }
+}
---
+++ ch3__7.java
@@ -0,0 +1,9 @@
+public class ch3__7 {
+    public static void main(String[] args){
+        float n=5.0f;
+        double p=n*n;
+        System.out.println(n+"*"+n+"="+p);
+
+    }
+
+}
